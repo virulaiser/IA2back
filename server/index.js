a467adae5aa4753ef677897e34e5615d6f17fa17
virulaiser <jsiutto@gmail.com>
2025-03-22 16:59:51 -0300

    ds

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-   res.status(200).json({ state: "SERVER ON" });
+  res.json({ estado: "API ON" });
 });
 
 app.use("/api/chat", chatRoutes);
