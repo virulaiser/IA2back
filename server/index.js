9f2d0c8e7d6087a13a5482a5fc4dd09822c641fb
virulaiser <jsiutto@gmail.com>
2025-03-22 16:40:37 -0300

    xas

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/",(req,res)=>{res.send("server ON")})
+app.get("/", (req, res) => {
+  res.send("<h1>Server On</h1>");
+});
 app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
